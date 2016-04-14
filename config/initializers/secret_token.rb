979b6d087176b99c154e0b8de980fe8b1382cab5
Patchx <robert.anderson.fl@gmail.com>
2016-04-14 14:13:11 -0400

    initial rails app with landing page and sessions scaffolded controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CareClone::Application.config.secret_key_base = 'b258a37e9b885491d97d2acbb0e654e9826d689fd4f8e1ff1939a1921d1d266e468b7cd780c41aa68d2e34cb54051feafe66f490d03a3033cf384647b14451a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CareClone::Application.config.session_store :cookie_store, key: '_careClone_session'
